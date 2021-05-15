4a8365f64273ee54dea6c06e0a023e474ba5dc0c
ChrisBlanks <christopherablanks@gmail.com>
2021-05-15 15:54:57 -0400

    change typcast

--- libc/string/memmove.c
+++ libc/string/memmove.c
@@ -2,7 +2,7 @@
 
 void* memmove(void* dstptr, const void* srcptr, size_t size){
     unsigned char* dst = (unsigned char*) dstptr;
-    const unsigned char* src = (const )srcptr;
+    const unsigned char* src = (const unsigned char* )srcptr;
 
     if(dst < src){
         for(size_t i = 0; i < size; i++){
