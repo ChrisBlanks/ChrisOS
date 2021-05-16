2b4fb4abfc92c721504fae182f8240e25684ec0d
ChrisBlanks <christopherablanks@gmail.com>
2021-05-16 19:40:45 -0400

    add kernel functions for displaying OS details

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -32,23 +32,14 @@ void renderColorfulASCIIArt(){
 }
 */
 
+void displayOSDetails(){
+    printf("\n---  Operating System Details ---\n");
+    printf("OS Name: ChrisOS\n");
+    printf("OS Creator: Chris Blanks\n");
+    printf("OS Architecture: i686\n\n");
+}
 
-//Kernel Entry Point
-void kernel_main(void){
-    /*
-    char* msg = "Hello, Kernel World!\n" 
-                "This will continue on for quite a while," 
-                "so that I can get a line break somewhere hahahahahahahahahaha\n";
-
-    char* long_msg = "1This will be a very long message that will hopefully test scrolling\n"
-                     "2second line: ffffffffffffffffffffffffffffffffffffffff\n"
-                     "3\n"
-                     "4\n5ffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-                     "\n6a\n7b\n8c\n9d\n10e\n11f\n12g\n13h\n14i\n15j\n16k\n17l\n18m\n19n\n20o\n21p"
-                     "fffffffffffffffffffffffffffffffffffffffff"
-                     "\n22q\n23r\n24s\n25t\n26ffff\n27after everything";
-    */
-
+void displayOSName(){
     char* ascii_art = 
    "  _____ _          _      ____   _____\n"
    " / ____| |        (_)    / __ \\ / ____|\n"
@@ -57,17 +48,18 @@ void kernel_main(void){
    "| |____| | | | |  | \\__ \\ |__| |____) |\n"
    " \\_____|_| |_|_|  |_|___/\\____/|_____/ \n";
 
+   printf(ascii_art);    
+}
+
+
+//Kernel Entry Point
+void kernel_main(void){
     terminalInitialize();
-    printf(ascii_art);
-    printf("\nHello, kernel world!");
+
+    displayOSName();
+    displayOSDetails();
     
-    //renderOSASCIIArt();
-    //renderColorfulASCIIArt();
-    
-    //test output
-    //terminalWriteString(msg);
-    //terminalWriteString(long_msg);
-    
+    return;
 }
 
 
