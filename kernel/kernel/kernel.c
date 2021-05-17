3258a3bb82cf96f67da31b9b3eec437af00c17cc
ChrisBlanks <christopherablanks@gmail.com>
2021-05-16 20:33:25 -0400

    remove commented out code in kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,35 +2,6 @@
 
 #include <kernel/tty.h>
 
-/*
-
-void renderOSASCIIArt(){
-    char* ascii_art = 
-   "  _____ _          _      ____   _____\n"
-   " / ____| |        (_)    / __ \\ / ____|\n"
-   "| |    | |__  _ __ _ ___| |  | | (___\n"  
-   "| |    | '_ \\| '__| / __| |  | |\\___ \n" 
-   "| |____| | | | |  | \\__ \\ |__| |____) |\n"
-   " \\_____|_| |_|_|  |_|___/\\____/|_____/ \n";
-
-    terminal_color = vgaEntryColor(VGA_COLOR_RED,VGA_COLOR_BLACK);
-    terminalWriteString(ascii_art);
-}
-
-void renderColorfulASCIIArt(){
-
-    char* ascii_art = 
-   "  _____ _          _      ____   _____\n"
-   " / ____| |        (_)    / __ \\ / ____|\n"
-   "| |    | |__  _ __ _ ___| |  | | (___\n"  
-   "| |    | '_ \\| '__| / __| |  | |\\___ \n" 
-   "| |____| | | | |  | \\__ \\ |__| |____) |\n"
-   " \\_____|_| |_|_|  |_|___/\\____/|_____/ \n";
-
-    terminalWriteColorful(ascii_art, strlen(ascii_art));
-
-}
-*/
 
 void displayOSDetails(){
     printf("\n---  Operating System Details ---\n");
