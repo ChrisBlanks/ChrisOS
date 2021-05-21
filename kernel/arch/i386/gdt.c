ded91d5828d5ce446d20dc4212bf1f4cc89ad697
ChrisBlanks <christopherablanks@gmail.com>
2021-05-20 22:49:03 -0400

    implement a simple global descriptor table

---
+++ kernel/arch/i386/gdt.c
@@ -0,0 +1,47 @@
+#include <kernel/gdt.h>
+
+#define DEFAULT_ENTRY_ARRAY_SIZE 5
+
+extern void gdt_flush(uint32_t); //defined in assembly file
+
+//global variables
+gdt_entry_t gdt_entries[DEFAULT_ENTRY_ARRAY_SIZE];
+gdt_ptr_t gdt_ptr;
+
+//static function prototypes
+static void createInitialGDTEntries(void);
+static void setGDTEntry(uint32_t index, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity);
+
+
+void initGDT(void){
+    createInitialGDTEntries();
+}
+
+
+static void createInitialGDTEntries(void){
+    gdt_ptr.limit = (sizeof(gdt_entry_t) * DEFAULT_ENTRY_ARRAY_SIZE) - 1;
+    gdt_ptr.base = (uint32_t)&gdt_entries;
+
+    setGDTEntry(0,0,0,0,0); //create null segment
+
+    //0xFFFFFFFF = 4 GiB
+    setGDTEntry(1,0,0xFFFFFFFF,0x9A,0xCF); //create code segment (CS), which tells the CPU which offset in the gdt has access privileges to execute code
+    setGDTEntry(2,0,0xFFFFFFFF,0x92,0xCF); //create data segment (DS), which tells the CPU the access privileges for the current data 
+    setGDTEntry(3,0,0xFFFFFFFF,0xFA,0xCF); //create user mode code segment
+    setGDTEntry(4,0,0xFFFFFFFF,0xF2,0xCF); //create user mode data segment
+
+    gdt_flush((uint32_t)&gdt_ptr);
+
+}
+
+static void setGDTEntry(uint32_t index, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity){
+    gdt_entries[index].base_low =  (  base & 0xFFFF); //get lower 16 bits
+    gdt_entries[index].base_mid =  ( (base >> 16) & 0xFF); //get mid 8 bit
+    gdt_entries[index].base_high = ( (base >> 24) & 0xFF); //get high 8 bits
+    
+    gdt_entries[index].limit_low = (limit & 0xFFFF); //get lower 16 bits
+    gdt_entries[index].granularity = ((limit >> 16) & 0x0F); //shift out lower 16 bits & mask off upper nibble to get the segment length
+    gdt_entries[index].granularity |= (granularity & 0xF0); //mask off lower 4 bits of granularity & OR it w/ previous operation
+    gdt_entries[index].access = access; 
+    
+}
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -12,10 +12,10 @@
 
 /*
  * Access byte format
- *  |    7    | 6 ... 5     | 4          | 3 ... 0 | 
- *  |segment  | Descriptor  | Descriptor | Segment |
- *  |present? | Privilege   | type       | Type    |
- *  |         | Level       |
+ *  |    7    | 6 ... 5     | 4          | 3          | 2              |  1     |   0       | 
+ *  |segment  | Descriptor  | Descriptor | Executable | Direction      |R/W bit | Accessed? |
+ *  |present? | Privilege   | type       | Bit        | or             |        | (reserved)|
+ *  |         | Level       |            |1 for enable| Conforming bit |        |
  *  |         | (ring 0-3)  |
  */  
 
@@ -28,20 +28,22 @@
  * 
  */  
 
+//limit = 20-bit value for indicating the maximum addressable unit
+// base = 32-bit value containing the lienar address where the segment begins
 
-typedef struct gdt_entry_struct {
+typedef struct __attribute__((packed)) gdt_entry_struct { //packed attribute retains alignment in structure
     uint16_t limit_low;  // lower 16-bits of the limit
     uint16_t base_low;   // lower 16 bits of the base
     uint8_t base_mid;    // middle 8 bits of base
     uint8_t access;      // access flags, which determine what ring the gdt segment can be used in
     uint8_t granularity; // describes segment configuration
     uint8_t base_high;   // last 8 bits of base
-}  gdt_entry_t __attribute__((packed)); //packed attribute retains alignment in structure
+}  gdt_entry_t; 
 
-typedef struct gdt_ptr_struct {
-    uint16_t limit; //high 16-bits of all selector values
+typedef struct __attribute__((packed)) gdt_ptr_struct { //packed attribute retains alignment in structure
+    uint16_t limit; // high 16-bits of all selector values
     uint32_t base;  // address of the 1st gdt_entry_t struct
-} gdt_ptr_t __attribute__((packed));
+} gdt_ptr_t;
 
 
 //function prototype
